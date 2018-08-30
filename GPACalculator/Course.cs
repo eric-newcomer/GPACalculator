d781094c0c3e87a61fac9b10888e73a717abf15d
eric-newcomer <ericnewcomer@microvu.com>
2018-08-30 12:20:53 -0700

    Add Course.cs and GPACalculator.cs

---
+++ GPACalculator/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPACalculator
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Grade { get; set; }
+        public int Credits { get; set; }
+
+        public Course(string Name, string Grade, int Credits)
+        {
+            this.Name = Name;
+            this.Grade = Grade;
+            this.Credits = Credits;
+        }
+    }
+}
--- GPACalculator/MainWindow.xaml.cs
+++ GPACalculator/MainWindow.xaml.cs
@@ -24,4 +24,5 @@ namespace GPACalculator
         {
             InitializeComponent();
         }
+    }
 }
