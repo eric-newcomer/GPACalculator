f05b474091648c494aeb8bafe74de420fe62ea50
eric-newcomer <ericnewcomer@microvu.com>
2018-08-30 14:29:30 -0700

    Add Calculate GPA button function

--- GPACalculator/MainWindow.xaml.cs
+++ GPACalculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,56 @@ namespace GPACalculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public class Course
+        {
+            public string Name { get; set; }
+            public string Grade { get; set; }
+            public double GPAGrade { get; set; }
+            public int Credits { get; set; }
+        }
+
+        public double GradeToGPA(string grade)
+        {
+            if (grade == "A")
+            {
+                return 4.0;
+            }
+            else if (grade == "B")
+            {
+                return 3.0;
+            }
+            else if (grade == "C")
+            {
+                return 2.0;
+            }
+            else if (grade == "D")
+            {
+                return 1.0;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+        public ObservableCollection<Course> Courses = new ObservableCollection<Course>();
+
+        public void CalculateGPA_Click(object sender, RoutedEventArgs e)
+        {
+            if (Class1Name != null)
+            {
+                Course course = new Course()
+                {
+                    Name = Class1Name.Text,
+                    Grade = Class1Grade.Text,
+                    GPAGrade = GradeToGPA(Class1Grade.Text),
+                    Credits = int.Parse(Class1Credits.Text)
+
+                };
+                Courses.Add(course);
+            }
+
         }
     }
 }
